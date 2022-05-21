125871420f55c832de1bb56c81cf7dc73ef3e133
AmalSakher <amalsakher.82@gmail.com>
2022-05-21 17:20:08 +0300

    updat

--- app/src/main/java/com/example/chatapp_cloud/lecturer/AddCourse.java
+++ app/src/main/java/com/example/chatapp_cloud/lecturer/AddCourse.java
@@ -1,6 +1,5 @@
 package com.example.chatapp_cloud.lecturer;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -73,7 +72,7 @@ public class AddCourse extends AppCompatActivity {
                       
                        Toast.makeText(AddCourse.this, "Added!", Toast.LENGTH_SHORT).show();
 
-                        startActivity(new Intent(AddCourse.this,HomePage.class));
+//                        startActivity(new Intent(AddCourse.this,HomePage.class));
                     }
 
                     @Override
